5ffa544038942d9dddf80f0bd76ff4c8ef17a9d8
Menghan Yang <menghan.yang@360learning.com>
2020-09-23 23:05:20 +0200

    Format files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Head from "next/head";
-import { getSortedPostsData } from "../lib/posts";
+import {getSortedPostsData} from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
 import {GetStaticProps} from "next";
@@ -14,12 +14,12 @@ export default function Home({allPostsData}) {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {allPostsData.map(({ id, date, title }) => (
+                    {allPostsData.map(({id, date, title}) => (
                         <li className={utilStyles.listItem} key={id}>
                             <Link href="/posts/[id]" as={`posts/${id}`}><a>{title}</a></Link>
-                            <br />
+                            <br/>
                             <small className={utilStyles.lightText}>
-                                <Date dateString={date} />
+                                <Date dateString={date}/>
                             </small>
                         </li>
                     ))}
