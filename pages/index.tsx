580412ff6e1977577ab6f1fbe317e11f883eaf88
Menghan Yang <menghan.yang@360learning.com>
2020-09-23 23:14:06 +0200

    Convert index page to TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,13 @@ import Link from "next/link";
 import Date from "../components/date";
 import {GetStaticProps} from "next";
 
-export default function Home({allPostsData}) {
+export default function Home({allPostsData}: {
+    allPostsData: {
+        date: string
+        title: string
+        id: string
+    }[]
+}) {
     return (
         <Layout home>
             <Head>…</Head>
