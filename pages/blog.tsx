2d20a54a5da9e7f88e6f8df06e82b3cc3b7f554e
Menghan Yang <menghan.yang@360learning.com>
2020-09-26 01:48:27 +0200

    Make create game page index

---
+++ pages/blog.tsx
@@ -0,0 +1,45 @@
+import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Head from 'next/head'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+import { GetStaticProps } from 'next'
+
+export default function Home ({ allPostsData }: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
+  return (
+    <Layout>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`posts/${id}`}><a>{title}</a></Link>
+              <br/>
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async context => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,14 @@
+import React from 'react'
 import Layout from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import Head from 'next/head'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
+import { Button, Input } from 'semantic-ui-react'
 
-export default function Home ({ allPostsData }: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
-}) {
+export default function CreateGame () {
   return (
     <Layout>
-      <Head>…</Head>
-      <section className={utilStyles.headingMd}>…</section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={`posts/${id}`}><a>{title}</a></Link>
-              <br/>
-              <small className={utilStyles.lightText}>
-                <Date dateString={date}/>
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <Input type={'text'} placeholder={'Your Name'} action>
+        <input/>
+        <Button type={'submit'} color={'teal'}>Start</Button>
+      </Input>
     </Layout>
   )
-}
-
-export const getStaticProps: GetStaticProps = async context => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
 }
