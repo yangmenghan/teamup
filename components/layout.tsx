1406aaef4fdac8498e742db4601b4653e53a0830
Menghan Yang <menghan.yang@360learning.com>
2020-09-26 19:12:11 +0200

    Remove Semantic components

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import React from 'react'
-import { Container, Grid, GridColumn } from 'semantic-ui-react'
 
 export const siteTitle = 'Undercover Online'
 
@@ -19,15 +18,8 @@ export default function Layout ({ children }: {
         <meta name="og:title" content={siteTitle}/>
         <title>{siteTitle}</title>
       </Head>
-      <Grid verticalAlign='middle' centered style={{ height: '100vh' }}>
-        <GridColumn>
-          <Container textAlign={'center'}>
-            <h1>{siteTitle}</h1>
-            <br/>
-            <main>{children}</main>
-          </Container>
-        </GridColumn>
-      </Grid>
+      <h1>{siteTitle}</h1>
+      <main>{children}</main>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import 'semantic-ui-css/semantic.min.css'
 import { AppProps } from 'next/app'
 
 export default function App ({ Component, pageProps }: AppProps) {
