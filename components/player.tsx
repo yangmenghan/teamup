a3e720f33c6422f3572323e13c93455efdca9cc2
Menghan Yang <menghan.yang@360learning.com>
2020-09-28 22:32:51 +0200

    Add current player block visibility check

--- components/player.tsx
+++ components/player.tsx
@@ -9,10 +9,12 @@ class PlayerProp {
 }
 
 export default function Player (playerProp: PlayerProp) {
+  const currentPlayer = <p className={style.currentPlayer}>Current player</p>
+  const currentPlayerContainerStyle = playerProp.isCurrentPlayer ? style.currentPlayerContainer : null
   return (
     <div className={style.playerContainer}>
-      <p className={style.currentPlayer}>Current player</p>
-      <div className={style.currentPlayerContainer}>
+      {playerProp.isCurrentPlayer ? currentPlayer : null}
+      <div className={currentPlayerContainerStyle}>
         <div className={style.profile}>
           <p className={style.nameInitial}>{playerProp.name.substring(0, 1)}</p>
         </div>
