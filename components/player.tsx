1cb0d7155559e0abab06f813a92fecf264fc4d95
Menghan Yang <menghan.yang@360learning.com>
2020-09-28 22:22:28 +0200

    Add PlayerProp

--- components/player.tsx
+++ components/player.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import style from './player.module.scss'
 
-export default function Player () {
+class PlayerProp {
+  id: number
+  name: string
+  isCurrentPlayer: boolean
+  lastWord?: string
+}
+
+export default function Player (playerProp: PlayerProp) {
   return (
     <div className={style.playerContainer}>
       <p className={style.currentPlayer}>Current player</p>
       <div className={style.currentPlayerContainer}>
         <div className={style.profile}>
-          <p className={style.nameInitial}>T</p>
+          <p className={style.nameInitial}>{playerProp.name.substring(0, 1)}</p>
         </div>
-        <p className={style.userName}>Name</p>
+        <p className={style.userName}>{playerProp.name}</p>
       </div>
-      <p className={style.word}>Word</p>
+      <p className={style.word}>{playerProp.lastWord}</p>
     </div>
   )
 }
