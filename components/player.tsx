aeaa751a4452552245e3226703df265fe0a36ef9
Menghan Yang <menghan.yang@360learning.com>
2020-09-30 00:02:50 +0200

    Adapter Player component

--- components/player.tsx
+++ components/player.tsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import style from './player.module.scss'
 import { PlayerEntity } from '../lib/game'
+import { CurrentPlayerLayout } from './currentPlayerLayout'
 
 export class PlayerProp {
   player: PlayerEntity
 }
 
 export default function Player (prop: PlayerProp) {
-  const currentPlayer = <p className={style.currentPlayer}>Current player</p>
-  const currentPlayerContainerStyle = prop.player.isCurrentPlayer ? style.currentPlayerContainer : null
+  const player = prop.player
+
   return (
     <div className={style.playerContainer}>
-      {prop.player.isCurrentPlayer ? currentPlayer : null}
-      <div className={currentPlayerContainerStyle}>
-        <div className={style.profile}>
-          <p className={style.nameInitial}>{prop.player.name.substring(0, 1)}</p>
-        </div>
-        <p className={style.userName}>{prop.player.name}</p>
-      </div>
-      <p className={style.word}>{prop.player.lastWord}</p>
+      <CurrentPlayerLayout isCurrentPlayer={player.isCurrentPlayer}>
+        <UserProfile player={player}/>
+      </CurrentPlayerLayout>
+      <p className={style.word}>{player.lastWord}</p>
     </div>
   )
 }
+
+function UserProfile (prop: PlayerProp) {
+  return <>
+    <div className={style.profile}>
+      <p className={style.nameInitial}>{prop.player.name.substring(0, 1)}</p>
+    </div>
+    <p className={style.userName}>{prop.player.name}</p>
+  </>
+}
+
