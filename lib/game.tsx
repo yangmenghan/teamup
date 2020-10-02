40300669281e7ca7d2eca359983515d5260de137
Menghan Yang <menghan.yang@360learning.com>
2020-10-02 15:13:36 +0200

    #1 - Add game related entity classes

--- lib/game.tsx
+++ lib/game.tsx
@@ -6,11 +6,28 @@ interface GameChangeListener {
   onGameChange (players: Array<PlayerEntity>)
 }
 
+export class GameEntity {
+  isStarted: boolean
+  words?: Array<WordEntity>
+  players: Array<PlayerEntity>
+  plays?: Map<string, Array<PlayEntity>>
+}
+
+export class WordEntity {
+  civilian: string
+  undercover: string
+}
+
 export class PlayerEntity {
   id: number
   name: string
   isCurrentPlayer: boolean
   lastWord?: string
+}
+
+export class PlayEntity {
+  votes: Map<string, string>
+  words: Map<string, string>
 }
 
 export function subscribeToGameChange (onGameChangeListener: GameChangeListener) {
