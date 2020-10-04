2be04821b1f449e6808920a03ef8c4a258d3e74f
Menghan Yang <menghan.yang@360learning.com>
2020-10-04 13:30:23 +0200

    #1 - Add create game route

---
+++ pages/api/game/create.tsx
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { v4 as uuidV4 } from 'uuid'
+import { db } from '../../../lib/firebase'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  createGame(req.body.userName, getGameCreatedCallback(res))
+
+  function createGame (userName: string, onGameCreated: (gameId: string, userId: string) => void) {
+    const gameId = generateGameId()
+    const userId = generateUserId()
+    const newGame = getNewGame(userId, userName)
+    db.ref('games/' + gameId).set(newGame).then(r => onGameCreated(gameId, userId))
+  }
+
+  function generateGameId () {
+    return uuidV4().substring(0, 5)
+  }
+
+  function generateUserId () {
+    return uuidV4()
+  }
+
+  function getNewGame (userId, userName: string) {
+    return {
+      players: [getNewUser(userId, userName)],
+      words: [],
+      plays: [],
+      isStarted: false
+    }
+  }
+
+  function getNewUser (userId: string, userName: string) {
+    return {
+      id: userId,
+      name: userName,
+      isCurrentPlayer: false,
+      lastWord: null
+    }
+  }
+
+  function getGameCreatedCallback (res: NextApiResponse) {
+    return (gameId: string, userId: string) => {
+      res.status(200)
+        .json({
+          game: gameId,
+          userId: userId
+        })
+    }
+  }
+}
