0f93e73ae2c39e12938ecaafbe0ea812e4efd622
Menghan Yang <menghan.yang@360learning.com>
2020-10-06 00:42:09 +0200

    #1 - Add routing to game page

--- components/createGame.tsx
+++ components/createGame.tsx
@@ -1,23 +1,38 @@
 import styles from './createGame.module.scss'
 import React from 'react'
 import { siteSubtitle, siteTitle } from './layout'
+import { withRouter } from 'next/router'
+import { WithRouterProps } from 'next/dist/client/with-router'
 
-interface CreateGameProps {
-  onStartClick: (name: string) => void
-}
+interface CreateGameProps extends WithRouterProps {}
 
 interface CreateGameState {
   name: string
 }
 
-export default class CreateGame extends React.Component<CreateGameProps, CreateGameState> {
-  constructor (prop) {
+class CreateGame extends React.Component<CreateGameProps, CreateGameState> {
+  constructor (prop: CreateGameProps) {
     super(prop)
     this.state = { name: '' }
   }
 
-  private handleChange = (event) => {
+  private handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
     this.setState({ name: event.target.value })
+  }
+
+  private handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const response = await fetch('/api/game/create', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ userName: this.state.name }),
+    })
+    const json = await response.json()
+    await this.props.router.push('/game/' + json.gameId)
   }
 
   render () {
@@ -25,7 +40,7 @@ export default class CreateGame extends React.Component<CreateGameProps, CreateG
       <div className={styles.card}>
         <h1 className={styles.title}>{siteTitle}</h1>
         <h4 className={styles.subtitle}>{siteSubtitle}</h4>
-        <form action="/api/game/create" method="POST">
+        <form onSubmit={this.handleSubmit}>
           <input type={'text'} required name={'userName'} placeholder={'Your Name'} onChange={this.handleChange}/>
           <div><input type={'submit'} value={'Start Game'}/></div>
         </form>
@@ -33,3 +48,5 @@ export default class CreateGame extends React.Component<CreateGameProps, CreateG
     )
   }
 }
+
+export default withRouter<CreateGameProps>(CreateGame)
