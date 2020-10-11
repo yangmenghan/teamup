404e3934501468b0a82e71e58ed82e3a2fcb1d7e
Menghan Yang <menghan.yang@360learning.com>
2020-10-12 00:50:40 +0200

    #2 - Simplify create route

--- pages/api/game/create.tsx
+++ pages/api/game/create.tsx
@@ -4,22 +4,17 @@ import { db } from '../../../lib/shared/utils/firebase'
 import { createNewGame } from '../../../lib/backend/util/entityFactory'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  createGame(req.body.userName, getGameCreatedCallback(res))
+  return createGame(req.body.userName, res)
 
-  function createGame (userName: string, onGameCreated: (gameId: string, userId: string) => void) {
+  async function createGame (userName: string, res: NextApiResponse) {
     const gameId = uuidV4().substring(0, 5)
     const userId = uuidV4()
     const newGame = createNewGame(userId, userName)
-    db.ref('games/' + gameId).set(newGame).then(r => onGameCreated(gameId, userId))
+    await db.ref('games/' + gameId).set(newGame)
+    sendResult(res, {gameId, userId})
   }
 
-  function getGameCreatedCallback (res: NextApiResponse) {
-    return (gameId: string, userId: string) => {
-      res.status(200)
-        .json({
-          gameId: gameId,
-          userId: userId
-        })
-    }
+  function sendResult (res: NextApiResponse, body : Object) {
+    res.status(200).json(body)
   }
 }
